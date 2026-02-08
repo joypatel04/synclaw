35d191d191955a02061c2b3d36bf11738dbab1f8
joypatel04 <joypatel041994@gmail.com>
2026-02-09 02:57:21 +0530

    Updated UI

--- app/page.tsx
+++ app/page.tsx
@@ -8,19 +8,17 @@ import { ActivityFeed } from "@/components/dashboard/ActivityFeed";
 function DashboardContent() {
   return (
     <div className="flex flex-col lg:flex-row h-[calc(100vh-3.5rem)]">
+      {/* Left sidebar: Agents */}
+      <div className="w-full lg:w-[280px] border-b lg:border-b-0 lg:border-r border-border-default bg-bg-secondary overflow-hidden">
+        <AgentPanel />
+      </div>
       {/* Main area: Kanban */}
       <div className="flex-1 overflow-auto p-6">
         <KanbanBoard />
       </div>
-      {/* Right sidebar */}
-      <div className="w-full lg:w-[320px] flex flex-col border-t lg:border-t-0 lg:border-l border-border-default bg-bg-secondary">
-        <div className="flex-1 overflow-hidden">
-          <AgentPanel />
-        </div>
-        <div className="h-px bg-border-default" />
-        <div className="flex-1 overflow-hidden">
-          <ActivityFeed />
-        </div>
+      {/* Right sidebar: Activity */}
+      <div className="w-full lg:w-[300px] border-t lg:border-t-0 lg:border-l border-border-default bg-bg-secondary overflow-hidden">
+        <ActivityFeed />
       </div>
     </div>
   );
